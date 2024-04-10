ee1a335649c5f8c5f224767cd3516f6a8790720e
parv nema <115030244+PARVNEMA@users.noreply.github.com>
2024-04-10 17:13:06 +0530

    Update Crypto.js

--- src/pages/Crypto.js
+++ src/pages/Crypto.js
@@ -5,7 +5,7 @@ import Filters from "../components/Filters";
 import { Outlet } from "react-router-dom";
 const Crypto = () => {
   return (
-    <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+    <section className="w-[90%] h-full flex flex-col mt-16 mb-24 relative">
       <Filters/>
       <TableComponent />
       <Outlet/>
